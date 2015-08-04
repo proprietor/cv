914260738066d3bf04b5977e806a8e9b77b56f1c
MEgor <eguar@tut.by>
2015-08-04 16:50:40 +0300

    Added pdf resume and PdfConverter project

---
+++ PdfConverter/PdfConverter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+
+public class PdfcrowdTest
+{
+    static void Main()
+    {
+        try
+        {
+            FileStream fileStream;
+
+            // create an API client instance
+            pdfcrowd.Client client = new pdfcrowd.Client("egor", "281dcd338e127d30472ea7e82b77ccad");
+
+            // convert a web page and save the PDF to a file
+            //fileStream = new FileStream("google_com.pdf", FileMode.CreateNew);
+            //client.convertURI("http://www.google.com", fileStream);
+            //fileStream.Close();
+
+            //// convert an HTML string and store the PDF into a memory stream
+            //MemoryStream memStream = new MemoryStream();
+            //string html = "<head></head><body>My HTML Layout</body>";
+            //client.convertHtml(html, memStream);
+
+            // convert an HTML file
+            fileStream = new FileStream(@"C:\Users\Egor\Desktop\cv\resume.pdf", FileMode.CreateNew);
+            client.convertFile(@"C:\Users\Egor\Desktop\cv\resume.html", fileStream);
+            fileStream.Close();
+
+            // retrieve the number of tokens in your account
+            //int ntokens = client.numTokens();
+        }
+        catch (pdfcrowd.Error why)
+        {
+            System.Console.WriteLine(why.ToString());
+        }
+    }
+}
